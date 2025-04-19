23ea8efe825e035ac4420edb1d202b8c5eae0db0
Himanshu Dhoble <120731707+HDhoble20@users.noreply.github.com>
2025-04-19 14:18:16 +0530

    Create arithmetic.mjs

---
+++ Web_Based_Technologies/JavaScript/AdvanceCodes/arithmetic.mjs
@@ -0,0 +1,31 @@
+
+export const myName="Himanshu Dhoble"
+{
+
+}
+
+export function addition(num1,num2)
+{
+ return `${num1}+${num2}=${num1+num2}`;
+};
+
+
+export function subtraction(num1,num2)
+{
+ return `${num1}-${num2}=${num1-num2}`;
+};
+
+
+export function multiplication(num1,num2)
+{
+ return `${num1}*${num2}=${num1*num2}`;
+};
+
+
+export function division(num1,num2)
+{
+ return `${num1}/${num2}=${num1/num2}`;
+};
+
+
+// console.log(addition(20,10));
