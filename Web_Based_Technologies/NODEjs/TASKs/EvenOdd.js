47485ae29b6dfc1b9bfc59e36d59f6ef7412312f
Himanshu Dhoble <120731707+HDhoble20@users.noreply.github.com>
2025-04-22 17:44:13 +0530

    Add files via upload

---
+++ Web_Based_Technologies/NODEjs/TASKs/EvenOdd.js
@@ -0,0 +1,11 @@
+function EvenOdd(num){
+    if(num%2==0)
+    {
+        return `${num} is even number`;
+    }
+    else
+    {
+        return `${num} is odd number`;
+    }
+}
+module.exports={EvenOdd}
