5bc0e63d7512e47ac78b60546b3eb716ab45865b
Himanshu Dhoble <120731707+HDhoble20@users.noreply.github.com>
2025-04-22 14:06:16 +0530

    Create arithmaticModule.js

---
+++ Web_Based_Technologies/NODEjs/arithmaticModule.js
@@ -0,0 +1,7 @@
+const arithmatic=require ('./arithmatic');
+const AOC=require('./AOC')
+console.log(arithmatic.add(700,300));
+console.log(arithmatic.sub(700,300));
+console.log(arithmatic.mul(70,30));
+console.log(arithmatic.div(200,4));
+console.log(AOC.AOC(3.14,5));
