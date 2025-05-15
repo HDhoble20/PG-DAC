a48514fe33d520f445600dffe042b8da5737e279
Himanshu Dhoble <120731707+HDhoble20@users.noreply.github.com>
2025-05-15 16:44:36 +0530

    Add files via upload

---
+++ WEB_BASED_JAVA/RequestDispatcherr/src/main/java/com/app/Servlet2.java
@@ -0,0 +1,46 @@
+package com.app;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Servlet2
+ */
+@WebServlet(urlPatterns = { "/serv2"})
+public class Servlet2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public Servlet2() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+		// TODO Auto-generated method stub
+				response.setContentType("text/html");
+				PrintWriter pw= response.getWriter();
+				pw.write("Welcome"+request.getParameter("txtUsername"));
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
